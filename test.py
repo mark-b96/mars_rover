ed5c663fe4c6b4c444402e1a2ff8863dade0311c
Mark Bonney <mark_b96@protonmail.com>
2018-12-05 12:40:25 +0200

    application files

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import unittest
 from Rover import Rover
+i = Rover()
+i.read_file("input.txt")
 
 
 class TestCornerCases(unittest.TestCase):
     def test_different_map(self):
-        i = Rover()
-        i.read_file("input.txt")
-        # i.x_bounds = 3
-        # i.y_bounds = 3
-        self.assertEqual(str(i.initialise_rover()), '1 3 N \n 5 1 E')
+        i.x_bounds = 3
+        i.y_bounds = 3
+        self.assertEqual(str(i.initialise_rover()), '13N\n31E\n')
+        i.output = []
+        i.x_bounds = 5
+        i.y_bounds = 5
+        self.assertEqual(str(i.initialise_rover()), '13N\n51E\n')
+
+    def test_out_of_bounds(self):
+        i.output = []
+        i.x_bounds = 2
+        i.y_bounds = 2
+        self.assertEqual(str(i.initialise_rover()), '12N\nInitial rover coordinates out of plateau bounds')
 
 if __name__ == '__main__':
     unittest.main()
